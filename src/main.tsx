3f1cab3be692d0325bda2352fdffd2bc9aec7e04
Usama Rauf <usamarauf93@gmail.com>
2025-06-27 06:02:43 +0500

    fix css

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,9 @@ import './styles/responsive/mediaquery600px.css'
 import './styles/responsive/mediaquery1024.css'
 import './styles/responsive/mediaquery1440.css'
 
+// Import responsive fixes
+import './styles/responsive-fixes.css'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
